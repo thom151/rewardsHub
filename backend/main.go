0787a795e10530319e031901f0ccbdb85c559c85
thom151 <thomassantos2003@gmail.com>
2026-02-17 17:48:56 +1100

    added server code

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port : %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
