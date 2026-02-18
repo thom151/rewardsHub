02f0b23a4542f273cb5a980aac3a8f69295febea
thom151 <thomassantos2003@gmail.com>
2026-02-18 13:01:42 +1100

    using neon database

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/thom151/rewardsHub/internal/database"
 )
+
+type apiConfig struct {
+	db *database.Queries
+}
 
 func main() {
 
 	const port = "8080"
+	godotenv.Load()
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL must be set")
+	}
 
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConn)
+
+	apiCfg := apiConfig{
+		db: dbQueries,
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /healthz", handlerReadiness)
+
+	mux.HandleFunc("POST /users", apiCfg.handlerCreateUser)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
